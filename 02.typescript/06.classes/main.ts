4d8099da5b0d80e62480f726eeca1be0ccab3dab
mohamed.aboelmagd <mohamed.aboelmagd@assesstm.com>
2017-11-23 12:29:02 +0200

    object

--- 02.typescript/06.classes/main.ts
+++ 02.typescript/06.classes/main.ts
@@ -1,12 +1,19 @@
 class Point {
-    x: number;
-    y: number;
+    private x: number;
+    private y: number;
 
+    constructor(x?: number, y?: number){
+        this.x = x;
+        this.y = y;
+    }
     draw() {
-        // ...
+        console.log('X: ' + this.x + ' Y: ' + this.y);       
     }
 
     getDistance(another: Point) {
         // ...
     }
 }
+
+let point = new Point(1, 2);
+point.draw();
