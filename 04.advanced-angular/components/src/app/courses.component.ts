1aca4d6b4d9168f0e64afd0cd54642c76a6d1240
Mohamed Aboelmagd <mohamed.aboelmagd7@gmail.com>
2017-11-24 23:27:51 +0200

    attribute binding

--- 04.advanced-angular/components/src/app/courses.component.ts
+++ 04.advanced-angular/components/src/app/courses.component.ts
@@ -6,11 +6,18 @@ import { Component } from '@angular/core';
                 <h2>{{ title }}</h2>
                 <h2 [textContent]="title"></h2>
                 <img src="{{ imageURL }}"/>
-                <img [src]="imageURL"/>`
+                <img [src]="imageURL"/>
+                
+                <table>
+                    <tr>
+                        <td [attr.colspan]="colSpan">25</td>
+                    </tr>
+                </table>`
 })
 export class CoursesComponent {
     title = 'List of courses';
     imageURL = 'http://lorempixel.com/400/400/';
+    colSpan = 2;
 
     constructor() {
 
