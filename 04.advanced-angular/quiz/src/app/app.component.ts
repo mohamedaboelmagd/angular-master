cc1ca5639eb510d3551276ca8e40c812fc9c5750
Mohamed Aboelmagd <mohamed.aboelmagd7@gmail.com>
2017-11-25 12:27:01 +0200

    quiz ppes

--- 04.advanced-angular/quiz/src/app/app.component.ts
+++ 04.advanced-angular/quiz/src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<span (click)="onClick()" class="glyphicon" [class.glyphicon-star]="star" [class.glyphicon-star-empty]="!star"></span>`,
-  // templateUrl: './app.component.html',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
