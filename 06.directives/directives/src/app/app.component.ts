f8fdc2c8f01ef1a808873eace3e322756e6cad2c
Mohamed Aboelmagd <mohamed.aboelmagd7@gmail.com>
2017-11-26 12:16:40 +0200

    ngFor

--- 06.directives/directives/src/app/app.component.ts
+++ 06.directives/directives/src/app/app.component.ts
@@ -9,4 +9,21 @@ export class AppComponent {
   courses = [1, 2];
 
   viewMode = 'map';
+
+  coursesObjs = [
+    { id: 1, name: 'course1'},
+    { id: 2, name: 'course2'},
+    { id: 3, name: 'course3'},
+  ];
+
+  onAdd() {
+    this.coursesObjs.push({ id: 4, name: 'course4' });
+  }
+  onRemove(course) {
+    const index = this.coursesObjs.indexOf(course);
+    this.coursesObjs.splice(index, 1);
+  }
+  onChange(course) {
+    course.name = 'UPDATED';
+  }
 }
