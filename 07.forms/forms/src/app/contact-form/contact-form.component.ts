8fa56d3443cd68634cb5677bed8611233ea79d83
Mohamed Aboelmagd <mohamed.aboelmagd7@gmail.com>
2017-11-26 15:22:22 +0200

    Drop-down list

--- 07.forms/forms/src/app/contact-form/contact-form.component.ts
+++ 07.forms/forms/src/app/contact-form/contact-form.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
-export class ContactFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class ContactFormComponent {
+  contactMethods = [
+    { id: 1, name: 'Email' },
+    { id: 2, name: 'Phone' },
+  ];
 
   log(attr) {
     console.log(attr);
