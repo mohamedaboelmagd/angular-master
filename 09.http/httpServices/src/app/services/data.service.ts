29aea1e7d74ec3afcb9736d18af29508ee2eda20
Mohamed Aboelmagd <mohamed.aboelmagd7@gmail.com>
2017-11-28 09:44:27 +0200

    Observables vs Promises

--- 09.http/httpServices/src/app/services/data.service.ts
+++ 09.http/httpServices/src/app/services/data.service.ts
@@ -6,6 +6,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/retry';
 import 'rxjs/add/observable/throw';
 
 @Injectable()
@@ -36,6 +38,8 @@ export class DataService {
     // return Observable.throw(new AppError());
     return this.http.delete(this.url + '/' + id)
         .map(response => response.json())
+        // .toPromise()
+        .retry(3)
         .catch(this.handleError);
   }
 
