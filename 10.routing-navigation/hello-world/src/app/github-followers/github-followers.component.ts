419c840d2776320b85566d4c3b774d3b7196d92b
Mohamed Aboelmagd <mohamed.aboelmagd7@gmail.com>
2017-11-28 12:28:07 +0200

    for seo and queryParams

--- 10.routing-navigation/hello-world/src/app/github-followers/github-followers.component.ts
+++ 10.routing-navigation/hello-world/src/app/github-followers/github-followers.component.ts
@@ -1,5 +1,6 @@
 import { GithubFollowersService } from './../services/github-followers.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router/src/router_state';
 
 @Component({
   selector: 'github-followers',
@@ -9,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 export class GithubFollowersComponent implements OnInit {
   followers: any[];
 
-  constructor(private service: GithubFollowersService) { }
+  constructor(private route: ActivatedRoute, private service: GithubFollowersService) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+
+    });
+    this.route.queryParamMap.subscribe(params => {
+
+    });
     this.service.getAll()
       .subscribe(followers => this.followers = followers);
   }
