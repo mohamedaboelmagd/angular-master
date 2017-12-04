ba6a46db5e8d9355179b1efbdba4dfedba2e4bd7
Mohamed Aboelmagd <mohamed.aboelmagd7@gmail.com>
2017-12-04 12:53:45 +0200

    Implementing a Fade-in Animation

--- 14.animations/animations-demo-start/src/app/todos/todos.component.ts
+++ 14.animations/animations-demo-start/src/app/todos/todos.component.ts
@@ -1,23 +1,33 @@
+import { trigger, transition, state, style, animate } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css'],
+  animations: [
+    trigger('fade', [
+      transition( 'void => *', [
+        style({ background: 'yellow', opacity: 0 }),
+        animate(2000)
+      ])
+    ])
+  ]
 })
 export class TodosComponent {
   items: any[] = [
-    'Wash the dishes', 
-    'Call the accountant', 
-    'Apply for a car insurance'];
+    'Wash the dishes',
+    'Call the accountant',
+    'Apply for a car insurance'
+  ];
 
   addItem(input: HTMLInputElement) {
     this.items.splice(0, 0, input.value);
-    input.value = ''; 
+    input.value = '';
   }
 
   removeItem(item) {
-    let index = this.items.indexOf(item);
+    const index = this.items.indexOf(item);
     this.items.splice(index, 1);
   }
 }
