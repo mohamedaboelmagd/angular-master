2f248db2ffd729d4cad38e7837cd40ee8b03ac8c
Mohamed Aboelmagd <mohamed.aboelmagd7@gmail.com>
2017-12-04 11:28:54 +0200

    updating an object

--- 13.firebase/firebase-demo/src/app/app.component.ts
+++ 13.firebase/firebase-demo/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireList } from 'angularfire2/database/interfaces';
-
+import 'rxjs/add/operator/do';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,18 +14,30 @@ export class AppComponent {
   author$;
 
   constructor(private db: AngularFireDatabase) {
-    this.courses$ = db.list('/courses').valueChanges();
+    this.courses$ = db.list('/courses').snapshotChanges()
+    .map(snap => snap.map(course => {
+      return { $key: course.key, value: course.payload.val()};
+    }));
     this.course$ = db.object('/courses/1').valueChanges();
     this.author$ = db.object('/authors/1').valueChanges();
+    this.courses$.do(console.log).subscribe();
   }
 
   add(course: HTMLInputElement) {
-    // this.db.list('/courses').push(course.value);
-    this.db.list('/courses').push({
-      name: course.value,
-      student: 1000,
-      isPremium: true
-    });
+    this.db.list('/courses').push(course.value);
+    // this.db.list('/courses').push({
+    //   name: course.value,
+    //   student: 1000,
+    //   isPremium: true
+    // });
     course.value = '';
+  }
+
+  update(course) {
+    this.db.object('/courses/' + course.$key)
+        // .set(course.value + ' Updated');
+        .update({
+          name: course.value.name ? course.value.name + ' Updated' : course.value + ' Updated'
+        });
   }
 }
