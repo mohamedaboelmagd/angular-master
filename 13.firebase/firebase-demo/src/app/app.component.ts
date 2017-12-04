a89fe19fe9bf1ffc63538369837453b62361c41d
Mohamed Aboelmagd <mohamed.aboelmagd7@gmail.com>
2017-12-04 10:24:35 +0200

    reading an object

--- 13.firebase/firebase-demo/src/app/app.component.ts
+++ 13.firebase/firebase-demo/src/app/app.component.ts
@@ -1,28 +1,19 @@
 import { Component } from '@angular/core';
-import { Subscription } from 'rxjs/Subscription';
 import { AngularFireDatabase } from 'angularfire2/database';
-import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnDestroy {
+export class AppComponent {
   courses$;
-  courses: any[];
-  subscription: Subscription;
+  course$;
+  author$;
 
   constructor(db: AngularFireDatabase) {
     this.courses$ = db.list('/courses').valueChanges();
-    this.subscription = db.list('/courses').valueChanges()
-    .subscribe(courses => {
-      this.courses = courses;
-      console.log(courses);
-    });
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.course$ = db.object('/courses/1').valueChanges();
+    this.author$ = db.object('/authors/1').valueChanges();
   }
 }
