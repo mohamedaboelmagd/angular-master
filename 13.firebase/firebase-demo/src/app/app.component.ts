56e87682d2c700f31895e5105a1a0c6b7f3fa716
Mohamed Aboelmagd <mohamed.aboelmagd7@gmail.com>
2017-12-04 10:09:00 +0200

    subscriptions and async

--- 13.firebase/firebase-demo/src/app/app.component.ts
+++ 13.firebase/firebase-demo/src/app/app.component.ts
@@ -1,20 +1,28 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
+  courses$;
   courses: any[];
+  subscription: Subscription;
 
   constructor(db: AngularFireDatabase) {
-    db.list('/courses').valueChanges()
+    this.courses$ = db.list('/courses').valueChanges();
+    this.subscription = db.list('/courses').valueChanges()
     .subscribe(courses => {
       this.courses = courses;
       console.log(courses);
     });
+  }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
