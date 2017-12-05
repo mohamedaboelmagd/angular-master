0c0550e0c68a4167a3b23091e174ad4d4fc0277d
Mohamed Aboelmagd <mohamed.aboelmagd7@gmail.com>
2017-12-05 15:31:25 +0200

    Separation of Concerns

--- 14.animations/animations-demo-start/src/app/zippy/zippy.component.ts
+++ 14.animations/animations-demo-start/src/app/zippy/zippy.component.ts
@@ -1,32 +1,11 @@
+import { expandCollapse } from './zippy.components.animations';
 import { Component, Input } from '@angular/core';
-import { trigger, transition, state, style, animate, animation,
-  keyframes, useAnimation, query, animateChild, group, stagger } from '@angular/animations';
 
 @Component({
   selector: 'zippy',
   templateUrl: './zippy.component.html',
   styleUrls: ['./zippy.component.css'],
-  animations: [
-    trigger('expandCollapse', [
-      state('collapsed', style({
-        paddingTop: 0,
-        paddingBottom: 0,
-        height: 0,
-        opacity: 0
-      })),
-      transition('collapsed => expanded', [
-        animate('300ms ease-out', style({
-          paddingTop: '*',
-          paddingBottom: '*',
-          height: '*'
-        })),
-        animate('1s', style({ opacity: 1 }))
-      ]),
-      transition('expanded => collapsed', [
-        animate('300ms ease-in')
-      ]),
-    ])
-  ]
+  animations: [ expandCollapse ]
 })
 export class ZippyComponent  {
   @Input('title') title: string;
