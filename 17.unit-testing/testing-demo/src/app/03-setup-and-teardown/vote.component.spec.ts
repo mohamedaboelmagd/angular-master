1f7f2d5f62127318b485c852827bea52fcbe889f
Mohamed Aboelmagd <mohamed.aboelmagd7@gmail.com>
2017-12-10 08:25:28 +0200

    setup and teardown

--- 17.unit-testing/testing-demo/src/app/03-setup-and-teardown/vote.component.spec.ts
+++ 17.unit-testing/testing-demo/src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,21 @@
-import { VoteComponent } from './vote.component'; 
+import { VoteComponent } from './vote.component';
 
 describe('VoteComponent', () => {
-  it('', () => {
+  // const component: VoteComponent;
+
+  beforeEach(() => {
+    const component = new VoteComponent();
   });
 
-  it('', () => {
+  it('it should increment after upvoted', () => {
+    this.component.upVote();
+
+    expect(this.component.totalVotes).toBe(1);
+  });
+
+  it('it should decrement after downvoted', () => {
+    this.component.downVote();
+
+    expect(this.component.totalVotes).toBe(-1);
   });
 });
