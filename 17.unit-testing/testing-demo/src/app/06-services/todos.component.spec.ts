7a0a28cb4d2da4dfd60510262bce36a95ef8adde
Mohamed Aboelmagd <mohamed.aboelmagd7@gmail.com>
2017-12-10 10:00:20 +0200

    Working with Spies

--- 17.unit-testing/testing-demo/src/app/06-services/todos.component.spec.ts
+++ 17.unit-testing/testing-demo/src/app/06-services/todos.component.spec.ts
@@ -1,12 +1,28 @@
-import { TodosComponent } from './todos.component'; 
-import { TodoService } from './todo.service'; 
+import { TodosComponent } from './todos.component';
+import { TodoService } from './todo.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/from';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
+  let service: TodoService;
 
   beforeEach(() => {
+    service = new TodoService(null);
+    component = new TodosComponent(service);
   });
 
-  it('', () => {
+  it('should return from fake backend', () => {
+    // Arrange
+    const todos = [1, 2, 3];
+    spyOn(service, 'getTodos').and.callFake(() => {
+      return Observable.from([ todos ]);
+    });
+
+    // Act
+    component.ngOnInit();
+
+    // Assert
+    expect(component.todos).toBe(todos);
   });
 });
