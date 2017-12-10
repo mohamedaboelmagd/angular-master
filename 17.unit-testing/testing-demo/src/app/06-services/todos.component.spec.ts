07b2846886680e0ab066ddffe1bf1dff66f4cd55
Mohamed Aboelmagd <mohamed.aboelmagd7@gmail.com>
2017-12-10 10:23:23 +0200

    Interaction Testing

--- 17.unit-testing/testing-demo/src/app/06-services/todos.component.spec.ts
+++ 17.unit-testing/testing-demo/src/app/06-services/todos.component.spec.ts
@@ -2,6 +2,8 @@ import { TodosComponent } from './todos.component';
 import { TodoService } from './todo.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/empty';
+import 'rxjs/add/observable/throw';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
@@ -24,5 +26,33 @@ describe('TodosComponent', () => {
 
     // Assert
     expect(component.todos).toBe(todos);
+  });
+
+  it('should check add method from service', () => {
+    const spy = spyOn(service, 'add').and.callFake((td) => {
+      return Observable.empty();
+    });
+
+    component.add();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should add item', () => {
+    const todo = { id: 1 };
+    const spy = spyOn(service, 'add').and.returnValue(Observable.from([ todo ]));
+
+    component.add();
+
+    expect(component.todos.indexOf(todo)).toBeGreaterThan(-1);
+  });
+
+  it('should throw error item', () => {
+    const error = 'error from server';
+    const spy = spyOn(service, 'add').and.returnValue(Observable.throw(error));
+
+    component.add();
+
+    expect(component.message).toBe(error);
   });
 });
