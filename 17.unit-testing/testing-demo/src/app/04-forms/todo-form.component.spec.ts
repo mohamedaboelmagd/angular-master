efda226c8a337744964f7c2e00cf3702fc4beada
Mohamed Aboelmagd <mohamed.aboelmagd7@gmail.com>
2017-12-10 09:14:14 +0200

    Working with form

--- 17.unit-testing/testing-demo/src/app/04-forms/todo-form.component.spec.ts
+++ 17.unit-testing/testing-demo/src/app/04-forms/todo-form.component.spec.ts
@@ -1,15 +1,23 @@
-import { TodoFormComponent } from './todo-form.component'; 
+import { FormBuilder } from '@angular/forms';
+import { TodoFormComponent } from './todo-form.component';
 
 describe('TodoFormComponent', () => {
-  var component: TodoFormComponent; 
+  let component: TodoFormComponent;
 
   beforeEach(() => {
-
+    component = new TodoFormComponent(new FormBuilder());
   });
 
-  it('', () => {
+  it('should has 2 inputs', () => {
+    expect(component.form.contains('name')).toBeTruthy();
+    expect(component.form.contains('email')).toBeTruthy();
   });
 
-  it('', () => {
+  it('name should make required', () => {
+    const control = component.form.get('name');
+
+    control.setValue('');
+
+    expect(control.valid).toBeFalsy();
   });
 });
